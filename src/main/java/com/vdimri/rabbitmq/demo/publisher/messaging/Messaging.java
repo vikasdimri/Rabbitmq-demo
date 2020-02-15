d32feb97fc2fcc61c6bbe89410645f69b264d7d2
Vikas Dimri <vikas.dimri@gmail.com>
2020-02-15 11:12:49 -0500

    Updated code

--- src/main/java/com/vdimri/rabbitmq/demo/publisher/messaging/Messaging.java
+++ src/main/java/com/vdimri/rabbitmq/demo/publisher/messaging/Messaging.java
@@ -1,7 +1,13 @@
 package com.vdimri.rabbitmq.demo.publisher.messaging;
 
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
 public interface Messaging {
 
-    String MY_MESSAGE= "my-message";
+    String MY_MESSAGE = "my-message";
+
+    @Output()
+    MessageChannel getMyMessageChannel();
 
 }
