ea6b8d7a3abf782fa14767ff2fca7f8e958cb5d7
Vikas Dimri <vikas.dimri@gmail.com>
2020-03-28 19:52:41 -0400

    Added prometheus

--- docker/dockerRun.sh
+++ docker/dockerRun.sh
@@ -2,3 +2,4 @@
 docker stop rabbit
 sleep 5
 docker run -d --rm --name rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker run -d --rm -p 9090:9090 -v /home/vikas/Projects/rabbitmq-demo/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
